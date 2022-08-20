b5ddd4b4e85fd9958077e013c0b97f36ee6e8d06
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-08-20 18:39:45 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,18 @@
 resource "google_compute_network" "vpc" {
-  name                    = local.vpc_name
+  name                    = var.vpc_name
   auto_create_subnetworks = "false"
 }
 
 resource "google_compute_subnetwork" "public" {
-  name          = "${local.vpc_name}-public-0"
-  region        = local.region
+  name          = "${var.vpc_name}-public-0"
+  region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.1.0.0/24"
 }
 
 resource "google_compute_subnetwork" "private" {
-  name                     = "${local.vpc_name}-private-0"
-  region                   = local.region
+  name                     = "${var.vpc_name}-private-0"
+  region                   = var.region
   private_ip_google_access = true
   network                  = google_compute_network.vpc.name
   ip_cidr_range            = "10.1.1.0/24"
