d35f6f8480b3832eda0ce345c0701dcd426acbc7
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-08-20 20:08:56 +0200

    Create gke_auth.tf

---
+++ gke_auth.tf
@@ -0,0 +1,12 @@
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  depends_on   = [module.gke]
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}-${timestamp()}"
+}
